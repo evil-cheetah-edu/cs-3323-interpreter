78d5b480932977ea7c0dc53907759a9ec03a3729
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-10-19 15:39:52 -0500

    add(docs): for resolve_int

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,3 +1,14 @@
+(** [resolve_int value] attempts to extract the [int] carried by the [Types.Integer].
+    @param value The [Types.value] to inspect.
+    @return [Some n] if [value] is [Types.Integer n]; [None] for any other type.
+    
+    Examples:
+    {[
+      resolve_int (Integer 42) = Some 42;
+      resolve_int (String "42") = None;
+      resolve_int Unit = None;
+    ]}
+*)
 let resolve_int (value: Types.value): int option =
     match value with
     | Integer number -> Some number
