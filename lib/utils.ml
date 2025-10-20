9d724907c88a71c2289bda31bae3c5adae000dca
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2025-10-19 21:08:13 -0500

    add(util): is valid name to check for var names

--- lib/utils.ml
+++ lib/utils.ml
@@ -41,3 +41,29 @@ let pp_value = function
     | Types.Unit          -> ":unit:"
     | Types.Error         -> ":error:"
 
+
+(** [is_valid_name variable_name] checks if a string is a legal variable name
+    according to the Assignment Specifications.
+
+    A valid variable name must follow to the regular expression:
+    "^[_]*[a-zA-Z][a-zA-Z_]*$"
+
+    Explanation:
+    - Start with zero or more underscores
+    - Immediately followed by at least one {letter}
+    - Be composed only of letters, numbers and underscores for the remainder
+    of the string
+
+    @param variable_name [string] to check if its name is valid
+    @return [true] if name is valid, [false] otherwise
+
+    For more information on legal constant and variable names, please, refer
+    to the {Assignment Specifications, Section 3.1 Constants}.
+*)
+let is_valid_name (variable_name: string): bool =
+    let pattern = Str.regexp "^[_]*[a-zA-Z][0-9a-zA-Z_]*$" in
+    try
+        let _ = Str.search_forward pattern variable_name 0 in
+        true
+    with
+    | Not_found -> false
